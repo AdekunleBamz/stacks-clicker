8226ecef2f984d18d7facad0262d9fb554e695d5
Adekunle Bamzz <bams.kunle@gmail.com>
2026-02-21 21:15:10 +0100

    feat: wrap app in AudioProvider and initialize global sound state

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { WalletProvider } from './context/WalletContext';
+import { AudioProvider } from './context/AudioContext';
 import App from './App.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <AudioProvider>
+      <WalletProvider>
+        <App />
+      </WalletProvider>
+    </AudioProvider>
   </React.StrictMode>,
 )
