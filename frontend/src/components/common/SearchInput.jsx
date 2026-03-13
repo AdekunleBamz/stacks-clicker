5240431381f30e1cd8947ff0623f67b3fc3e8f24
Adekunle Bamzz <68190511+AdekunleBamz@users.noreply.github.com>
2026-03-13 21:03:31 +0100

    Use non-submit button type for search clear control

--- frontend/src/components/common/SearchInput.jsx
+++ frontend/src/components/common/SearchInput.jsx
@@ -29,6 +29,7 @@ function SearchInput({ value, onChange, onClear, placeholder = 'Search...', coun
         <AnimatePresence>
           {value && (
             <motion.button
+              type="button"
               initial={{ opacity: 0, scale: 0.8 }}
               animate={{ opacity: 1, scale: 1 }}
               exit={{ opacity: 0, scale: 0.8 }}
