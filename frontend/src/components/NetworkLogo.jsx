ed7fdca6231c7d65cb7d73f9c64f7fcc0aea6c93
Bamzz <bams.kunle@gmail.com>
2026-03-22 21:47:57 +0100

    Remove conflicting SVG label metadata from decorative logo

--- frontend/src/components/NetworkLogo.jsx
+++ frontend/src/components/NetworkLogo.jsx
@@ -12,7 +12,7 @@ export default function NetworkLogo() {
       transition={{ duration: 4, repeat: Infinity, ease: "easeInOut" }}
       aria-hidden="true"
     >
-      <svg width="32" height="32" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Stacks Interactive Clicker Logo">
+      <svg width="32" height="32" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
         <path d="M16 2L4 16L16 30L28 16L16 2Z" fill="url(#logo-grad)" />
         <path d="M16 6L8 16L16 26L24 16L16 6Z" fill="white" fillOpacity="0.2" />
         <defs>
