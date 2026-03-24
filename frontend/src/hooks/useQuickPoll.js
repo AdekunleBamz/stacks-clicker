e272e4d9cdeaf1658a1ca4b73378cca13a649c50
Bamzz <bams.kunle@gmail.com>
2026-03-24 22:38:41 +0100

    refactor: use shared constants in useQuickPoll hook

--- frontend/src/hooks/useQuickPoll.js
+++ frontend/src/hooks/useQuickPoll.js
@@ -1,10 +1,6 @@
 import { useState, useCallback } from 'react';
 import { callContract } from '../utils/walletconnect';
-
-/** @constant {string} Smart contract deployer address */
-const DEPLOYER = 'SP5K2RHMSBH4PAP4PGX77MCVNK1ZEED07CWX9TJT';
-/** @constant {string} QuickPoll contract name */
-const CONTRACT_NAME = 'quickpoll-v2p';
+import { DEPLOYER, QUICKPOLL_CONTRACT as CONTRACT_NAME } from '../utils/constants';
 
 /**
  * Custom hook for interacting with the QuickPoll smart contract.
