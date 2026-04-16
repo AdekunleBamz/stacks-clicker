b440be5b0786d37e027164b930e7e51ec5e1530d
AdekunleBamz <bams.kunle@gmail.com>
2026-04-16 18:47:10 +0100

    replace non-standard style jsx with standard style tag

--- frontend/src/components/common/ButtonBadge.jsx
+++ frontend/src/components/common/ButtonBadge.jsx
@@ -35,7 +35,7 @@ const ButtonBadge = memo(function ButtonBadge({ count, showDotOnly = false, maxC
         </motion.div>
       )}
 
-      <style jsx>{`
+      <style>{`
         .button-badge {
           position: absolute;
           top: -4px;
