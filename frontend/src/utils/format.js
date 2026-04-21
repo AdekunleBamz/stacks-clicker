ebf85d265925812f6a5389a26697445231431f0c
AdekunleBamz <bams.kunle@gmail.com>
2026-04-21 14:34:32 +0100

    feat: add formatUpgradeName helper

--- frontend/src/utils/format.js
+++ frontend/src/utils/format.js
@@ -169,3 +169,5 @@ export const formatCombo = (n) => n + "x combo";
 export const formatPrestigeCount = (n) => "Prestige " + n;
 
 export const formatSessionTime = (ms) => Math.floor(ms / 60000) + "m " + Math.floor((ms % 60000) / 1000) + "s";
+
+export const formatUpgradeName = (key) => key.replace(/_/g, " ").toLowerCase();
