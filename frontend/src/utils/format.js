bb6f9d6f6ba302fc4d5f47863ce6c37ccf3eac44
AdekunleBamz <bams.kunle@gmail.com>
2026-04-21 14:34:27 +0100

    feat: add formatRank helper

--- frontend/src/utils/format.js
+++ frontend/src/utils/format.js
@@ -159,3 +159,5 @@ export const formatCost = (n) => Number(n).toLocaleString() + " pts";
 export const formatPercent = (n) => (Number(n) * 100).toFixed(1) + "%";
 
 export const formatDuration = (ms) => (ms / 1000).toFixed(1) + "s";
+
+export const formatRank = (n) => "#" + n;
