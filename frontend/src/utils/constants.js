25c81a73b63cf42e09796f292fccf65a13e67d09
AdekunleBamz <bams.kunle@gmail.com>
2026-04-26 07:20:13 +0100

    add BOOST_LEVEL_CAP upper limit for purchasable boosts

--- frontend/src/utils/constants.js
+++ frontend/src/utils/constants.js
@@ -129,6 +129,8 @@ export const NOTIFICATION_DISMISS_MS = 5000;
 
 export const AUTO_CLICKER_BASE_RATE = 1;
 
+export const BOOST_LEVEL_CAP = 5;
+
 export const SESSION_EXPIRY_MS = 3_600_000;
 
 export const AUTO_SAVE_INTERVAL_MS = 30_000;
