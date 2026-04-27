4a2f826152342ac5771e61d1c81c66104d8f2577
AdekunleBamz <bams.kunle@gmail.com>
2026-04-27 11:24:08 +0100

    refactor: remove unused React default import in Modal, ButtonBadge, PullToRefresh, SkeletonLoader

--- frontend/src/components/common/Modal.jsx
+++ frontend/src/components/common/Modal.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useCallback, memo } from 'react';
+import { useEffect, useCallback, memo } from 'react';
 import { motion, AnimatePresence } from 'framer-motion';
 import PropTypes from 'prop-types';
 
