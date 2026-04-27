97bd0b667f120d0b8c28e6c39364e8491c2553b2
AdekunleBamz <bams.kunle@gmail.com>
2026-04-27 08:31:44 +0100

    a11y: hide inner SVG from screen readers since parent has label

--- frontend/src/components/NetworkLogo.jsx
+++ frontend/src/components/NetworkLogo.jsx
@@ -28,7 +28,7 @@ function NetworkLogo({ isSyncing = false }) {
       aria-label="Stacks Clicker animated diamond logo"
       title="Stacks Clicker Network Logo"
     >
-      <svg width="32" height="32" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Stacks Clicker logo">
+      <svg width="32" height="32" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" focusable="false">
         <path d="M16 2L4 16L16 30L28 16L16 2Z" fill="url(#logo-grad)" />
         <path d="M16 6L8 16L16 26L24 16L16 6Z" fill="white" fillOpacity="0.2" />
         <defs>
