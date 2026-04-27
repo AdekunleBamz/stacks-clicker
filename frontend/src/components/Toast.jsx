fd313158fc5d62528e48b912e0a61a14096f6995
AdekunleBamz <bams.kunle@gmail.com>
2026-04-27 08:41:32 +0100

    perf: memoize Toast component export

--- frontend/src/components/Toast.jsx
+++ frontend/src/components/Toast.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { memo } from 'react';
 import { motion, AnimatePresence } from 'framer-motion';
 import PropTypes from 'prop-types';
 
@@ -49,4 +49,4 @@ Toast.propTypes = {
   ),
 };
 
-export default Toast;
+export default memo(Toast);
