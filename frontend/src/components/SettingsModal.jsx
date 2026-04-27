b96ffbf01d57b09e8332dda395efc02207f4462e
AdekunleBamz <bams.kunle@gmail.com>
2026-04-27 11:19:48 +0100

    refactor: remove unused React default import in SettingsModal, ProgressDashboard, Achievement

--- frontend/src/components/SettingsModal.jsx
+++ frontend/src/components/SettingsModal.jsx
@@ -1,4 +1,4 @@
-import React, { memo } from 'react';
+import { memo } from 'react';
 import Modal from './common/Modal';
 import { useModal } from '../context/ModalContext';
 import { useTheme } from '../hooks/useTheme';
