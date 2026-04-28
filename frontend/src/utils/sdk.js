47a1a718b72a0be665af5e8e547daf519d926d72
AdekunleBamz <bams.kunle@gmail.com>
2026-04-28 07:44:28 +0100

    Simplify SDK network init using normalized constant

--- frontend/src/utils/sdk.js
+++ frontend/src/utils/sdk.js
@@ -3,7 +3,7 @@ import { StacksClickerSDK } from 'stacks-clicker-sdk';
 import { STACKS_NETWORK } from './constants';
 
 export const stacksClickerSdk = new StacksClickerSDK({
-  network: STACKS_NETWORK === 'testnet' ? 'testnet' : 'mainnet',
+  network: STACKS_NETWORK,
 });
 
 /**
